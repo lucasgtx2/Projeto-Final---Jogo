b99c3be14b5d0469d34ec2629f091975271ac141
Lucas Teixeira <you@example.comlucasgmt@al.insper.edu.br>
2020-05-12 15:38:05 -0300

    config e assets carregados
    Co-authored-by: guilhermerc5@al.insper.edu.br <guilhermerc5@al.insper.edu.br>
    Co-authored-by: MatheusVenturi201 <matheusmv@al.insper.edu.br>

--- assets.py
+++ assets.py
@@ -1,1 +1,41 @@
-#codigo para definicao dos assets.
+#codigo para definicao dos assets
+import pygame
+import os
+from config import IMG_DIR, FNT_DIR, PINGUIMD_WIDTH, PINGUIMD_HEIGHT, PINGUIMP_WIDTH, PINGUIMP_HEIGHT, SALMAOC_WIDTH, SALMAOC_HEIGHT, SALMAOD_WIDTH, SALMAOD_HEIGHT, BOMBA_PEDRA_WIDTH, BOMBA_PEDRA_HEIGHT
+
+
+BACKGROUND = 'background_img'
+PINGUIMD_IMG = 'pinguim_deitado_img'
+PINGUIMP_IMG = 'pinguim_em_pe_img'
+SALMAOC_IMG = 'salmao_carne_img'
+SALMAOD_IMG = 'salmao_desenho_img'
+'''#ainda nao definimos o score
+SCORE_FONT = 'score_font'
+'''
+BOMBA_IMG = 'bomba_img'
+PEDRA_IMG = 'pedra_img'
+
+
+def load_assets():
+    assets = {}
+    assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'fundo neve igloo.jpg')).convert()
+    assets[PINGUIMD_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'pinguim deitado.jpg')).convert_alpha()
+    assets[PINGUIMD_IMG] = pygame.transform.scale(assets['pinguim_deitado_img'], (PINGUIMD_WIDTH, PINGUIMD_HEIGHT))
+    assets[PINGUIMP_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'pinguim em pe.jpg')).convert_alpha()
+    assets[PINGUIMP_IMG] = pygame.transform.scale(assets['pinguim_em_pe_img'], (PINGUIMP_WIDTH, PINGUIMP_HEIGHT))
+    assets[SALMAOC_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'salmao carne.jpg')).convert_alpha()
+    assets[SALMAOC_IMG] = pygame.transform.scale(assets['salmao_carne_img'], (SALMAOC_WIDTH, SALMAOC_HEIGHT))
+    assets[SALMAOD_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'salmao desenho.jpg')).convert_alpha()
+    assets[SALMAOD_IMG] = pygame.transform.scale(assets['salmao_desenho_img'], (SALMAOD_WIDTH, SALMAOD_HEIGHT))
+    assets[BOMBA_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'bomba desenho.jpg')).convert_alpha()
+    assets[BOMBA_IMG] = pygame.transform.scale(assets['bomba_img'], (SALMAOD_WIDTH, SALMAOD_HEIGHT))
+    assets[PEDRA_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'pedra desenho.jpg')).convert_alpha()
+    assets[PEDRA_IMG] = pygame.transform.scale(assets['pedra_img'], (SALMAOD_WIDTH, SALMAOD_HEIGHT))
+
+
+    '''# Carrega os sons do jogo
+    pygame.mixer.music.load(os.path.join(SND_DIR, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+    pygame.mixer.music.set_volume(0.4)
+    assets[BOOM_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl3.wav'))'''
+    
+    return assets
---
+++ config.py
@@ -0,0 +1,41 @@
+#configurações
+from os import path
+
+# Estabelece a pasta que contem as figuras e sons.
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'imagens')
+'''FALTA ESCOLHER SOM
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'som')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'fonte')
+'''
+# Dados gerais do jogo.
+WIDTH = 1024 # Largura da tela
+HEIGHT = 768 # Altura da tela
+FPS = 30 # Frames por segundo
+
+# Define tamanhos
+PINGUIMD_WIDTH = 80
+PINGUIMD_HEIGHT = 60
+PINGUIMP_WIDTH = 60
+PINGUIMP_HEIGHT = 80
+SALMAOC_WIDTH = 50
+SALMAOC_HEIGHT = 38
+SALMAOD_WIDTH = 50
+SALMAOD_HEIGHT = 38
+BOMBA_PEDRA_WIDTH = 50 
+BOMBA_PEDRA_HEIGHT = 50
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
+END = 3
+
+
